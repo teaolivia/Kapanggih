e611e78a8df63cf4c25c554e49ccc81b1d3a0eec
rifkiansyahmc <skyline_bell_cat@yahoo.com>
2015-03-26 22:20:06 +0700

    Pushed DFS. Please do check because I can't check.

--- DFS.cs
+++ DFS.cs
@@ -2,43 +2,133 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-namespace DepthFirst
+ 
+namespace DepthFirst // karena gw gabisa ngecek langsung cara kerjanya dengan compile dan run
+//gw tulis pake komen yang mungkin pas.
+//nama List<File> sama obyek yang diciptain dari situ masih gw isi ???
 {
-
-	public class Tree<K, V>
-		where K : class, IComparable<K>
-		where V : class
-	{
-		private Node<K, V> root;
+    class Program
+    {
+        public class File //File
+        {
+            public File(string name) //File (string name, string type?)
+            {
+                this.name = name; //this.name = name; this.type = type
+            }
  
-		public V DFS(K key) 
-		{
-			Stack<Node<K, V>> stack = new Stack<Node<K, V>>();
-        
-			while(stack.Any())
-			{
-				var node = stack.Pop();
+            public string name { get; set; } //name, type?
+            public List<File> files //List<File> ???
+            {
+                get
+                {
+                    return FileList; //FileList?
+                }
+            }
+             
+            public void isUnder(File f) //isUnder(File f)?
+            {
+                FileList.Add(f); //FileList.Add(f)
+            }
  
-				if(node.key == key)
-				{
-					return node.value;
-				}
-				foreach (var child  in node.children)
-				{
-					stack.Push(child);
-				}
-			}
-			return default(V);
-		}
+            List<File> FileList = new List<File>(); //List<File> FileList = new List<File>();
  
-		private class Node<K, V>
-			where K : class, IComparable<K>
-			where V : class
-		{
-			public K key;
-			public V value;
-			public Node<K, V>[] children;
-		}
-	}
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+ 
+        public class DepthFirstAlgorithm
+        {
+            public File BuildFileGraph() //File BuildFileGraph? ini kayaknya test dummy. Mungkin harus bikin yang search ke directory.
+            {
+                File Aaron = new File("Aaron");
+                File Betty = new File("Betty");
+                File Brian = new File("Brian");
+                Aaron.isUnder(Betty);
+                Aaron.isUnder(Brian);
+ 
+                File Catherine = new File("Catherine");
+                File Carson = new File("Carson");
+                File Darian = new File("Darian");
+                File Derek = new File("Derek");
+                Betty.isUnder(Catherine);
+                Betty.isUnder(Darian);
+                Brian.isUnder(Carson);
+                Brian.isUnder(Derek);
+ 
+                return Aaron;
+            } 
+ 
+            //http://en.wikipedia.org/wiki/Breadth-first_search#Pseudocode
+            public File Search(File root, string nameToSearchFor) //File root, string nameToSearchFor
+            {
+                Stack<File> S = new Stack<File>(); //Kita pakai queue atau stack? disini Queue... anyway, Queue<File>
+                HashSet<File> H = new HashSet<File>(); //HashSet gw belum gitu ngerti. Bikin HashSet<File> berarti kan.
+                S.Push(root); //tetep
+                H.Add(root);	//tetep
+ 
+                while (S.Count > 0)
+                {
+                    File f = S.Pop(); //File f = Q.Dequeue();
+                    if (f.name == nameToSearchFor) //f.name == nameToSearchFor
+                        return f;
+                    foreach (File files in f.FileList) //File ?? in f.???
+                    {
+                        if (!S.Contains(files)) // !S.Contains(??), ini maksud Containsnya contains salah satu relasi file mungkin?
+                        {
+                            S.Pop(files); // Q.Enqueue(??)
+                            H.Add(files); //S.Add(??)
+                        }
+                    }
+                }
+                return null;
+            }
+ 
+            public void Traverse(File root) //File root
+            {
+                Stack<File> traverseOrder = new Stack<File>(); //Queue<File> traverseOrder = new Queue<File>();
+ 
+                Stack<File> S = new Stack<File>(); // Queue<File>
+                HashSet<File> H = new HashSet<File>(); //HashSet<File>
+                S.push(root);
+                H.Add(root);
+ 
+                while (S.Count > 0)
+                {
+                    File f = S.Pop(); // File f;
+                    traverseOrder.Push(f); //Enqueue(f)
+ 
+                    foreach (File files in f.FileList) //File ?? in p.???
+                    {
+                        if (!S.Contains(files)) // !S.Contains(??)
+                        {
+                            S.Push(files); // Q.Enqueue(??)
+                            H.Add(files); //S.Add(??);
+                        }
+                    }
+                }
+ 
+                while (traverseOrder.Count > 0)
+                {
+                    Files f = traverseOrder.Pop(); //File f;
+                    Console.WriteLine(f); //(f)
+                }
+            }
+        }
+ 
+        static void Main(string[] args)
+        {
+            DepthFirst d = new DepthFirst();
+            File root = d.BuildFileGraph();
+            Console.WriteLine("Traverse\n------");
+            d.Traverse(root);
+ 
+            Console.WriteLine("\nSearch\n------");
+            File f = d.Search(root, "Catherine");
+            Console.WriteLine(f == null ? "Person not found" : f.name);
+            f = d.Search(root, "Alex");
+            Console.WriteLine(f == null ? "Person not found" : f.name);
+        }
+    }
 }
